5b4acfcf6071f7ec477ad276208ccd97040fa9fd
Nick Masella <142621287+NickMasella@users.noreply.github.com>
2023-08-20 15:33:44 -0500

    tinkered with code

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,7 +1,7 @@
    // This demo shows off comments!
 
    // console.log("This does not print.");
-
+   
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
    /* Here is how
@@ -9,4 +9,20 @@
    multi-line
    comments. */
 
-   console.log("Comments make your code more readable by others.");
+   /*This 
+   is 
+   a
+   comment
+   on
+   a
+   bunch
+   of
+   lines
+   for
+   no
+   reason
+   */
+
+   // Here is another comment
+
+   console.log("Comments should make your code more readable by others.");
--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,9 @@
 console.log('Hello, JavaScript.');
-console.log(2001);
+console.log(2001 +': A Space Odyssey');
 console.log("What","do","commas","do?");
+console.log("Commas","are","spaces");
 console.log("Does", "adding",      "space", "matter?");
+console.log("Space", "is",      "empty" + '/ingored');
 console.log('Launch' + 'Code');
-console.log("LaunchCode was founded in", 2013);
+console.log('+' , 'adds no space');
+console.log("LaunchCode was founded in", 2013, 'by Jim Mckelvey');
