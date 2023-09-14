c67d66d8cfe7c01739ceacdeb0cd9eb6ff670704
Nick Masella <142621287+NickMasella@users.noreply.github.com>
2023-09-13 19:01:17 -0500

    Space shuttle launch conditionals

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,15 @@
 // Declare and initialize the variables for exercise 1 here:
 
+let engineIndicatorLight =	"green blinking";
+let spaceSuitsOn	= true;
+let shuttleCabinReady =	true;
+let crewStatus	= (spaceSuitsOn && shuttleCabinReady);
+let computerStatusCode =	400;
+let shuttleSpeed	= 15000;
+
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+
+// The engine indicator is blinking red meaning the engines are off, which is what will be printed to the console
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
@@ -9,3 +18,59 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+// Confirmation of mission status
+
+if (crewStatus) {
+   console.log("Crew Ready");
+} else {
+   console.log("Crew Not Ready");
+}
+
+if (computerStatusCode === 200){
+   console.log("Please stand by. Computer is rebooting.");
+} else if (computerStatusCode === 400){
+   console.log("Success! Computer online.");
+} else {
+   console.log("ALERT: Computer offline!");
+}
+
+if (shuttleSpeed > 17500){
+   console.log("ALERT: Escape velocity reached!")
+} else if (shuttleSpeed < 8000){
+   console.log("ALERT: Cannot maintain orbit!")
+} else {
+   console.log("Stable speed")
+}
+
+let fuelLevel = 30000;
+let engineTemperature = 2000;
+
+if (fuelLevel < 1000 || engineTemperature > 3500){
+   console.log("!!!!ENGINE FAILURE IMMINENT!!!!");
+} else if (fuelLevel > 20000 && engineTemperature <= 2500){
+   console.log("Full tank. Engines good");
+} else if (fuelLevel > 10000 && engineTemperature <= 2500){
+   console.log("Fuel level above 50%. Engines good");
+} else if (fuelLevel > 5000 && engineTemperature <= 2500){
+   console.log("Fuel level above 25%. Engines good");
+} else if (fuelLevel <= 5000 || engineTemperature > 2500){
+   if (fuelLevel >= 5000 && engineTemperature > 2500){
+      console.log("Fuel level above 25%. Engines running hot.");
+   } else if (fuelLevel < 5000 && engineTemperature <= 2500){
+      console.log("Check fuel level. Engines good");
+   } else {
+   console.log("Check fuel level. Engines running hot");
+   }
+}
+
+let commandOverride = false;
+
+if (fuelLevel > 20000 && engineIndicatorLight !== "red blinking" || commandOverride == true){
+   console.log("Cleared for launch!");
+} else {
+   console.log("Launch scrubbed!");
+}
+
+
+
---
+++ booleans-and-conditionals/exercises/practice.js
@@ -0,0 +1,9 @@
+console.log(Boolean("true"));
+console.log(Boolean("TRUTH"));
+console.log(Boolean(""));
+console.log(Boolean(" "));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean('banana' <= 'apple'));
+console.log(Boolean('apple' <= 'banana'));
+console.log((3 + 4 === 7));
