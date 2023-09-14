b3575f41e56965db265825c0a8f356be4ba6b5a8
Nick Masella <142621287+NickMasella@users.noreply.github.com>
2023-09-13 19:27:09 -0500

    added code to proper files

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -11,37 +11,11 @@ let shuttleSpeed	= 15000;
 
 // The engine indicator is blinking red meaning the engines are off, which is what will be printed to the console
 
-if (engineIndicatorLight === "green") {
-   console.log("engines have started");
-} else if (engineIndicatorLight === "green blinking") {
-   console.log("engines are preparing to start");
-} else {
-   console.log("engines are off");
-}
+
 
 // Confirmation of mission status
 
-if (crewStatus) {
-   console.log("Crew Ready");
-} else {
-   console.log("Crew Not Ready");
-}
 
-if (computerStatusCode === 200){
-   console.log("Please stand by. Computer is rebooting.");
-} else if (computerStatusCode === 400){
-   console.log("Success! Computer online.");
-} else {
-   console.log("ALERT: Computer offline!");
-}
-
-if (shuttleSpeed > 17500){
-   console.log("ALERT: Escape velocity reached!")
-} else if (shuttleSpeed < 8000){
-   console.log("ALERT: Cannot maintain orbit!")
-} else {
-   console.log("Stable speed")
-}
 
 
 
