07e7bedeb92a71466f54cb60ca058c5d07ac8c1e
Nick Masella <142621287+NickMasella@users.noreply.github.com>
2023-09-28 17:01:23 -0500

    exercise

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,7 +5,14 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+let theJumbler = function(jumble){
+    if(typeof jumble === typeof ''){
+        return "ARRR!";
+    } else if (typeof jumble === typeof 0){
+        return jumble * 3;
+    }
 
+};
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
@@ -13,3 +20,5 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+console.log(arr.map(theJumbler));
